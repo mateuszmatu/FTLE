301900d3486f20e3238379b5024538867760363d
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-03-11 08:55:09 +0000

    removed redundant functions

--- main.py
+++ main.py
@@ -3,34 +3,6 @@ from FTLE import FTLE
 import os
 from datetime import datetime, timedelta, date
 
-def CreateLCSField(file, lons, lats, ts, sep, dur, output, start):
-    """
-        Advects particles using ParticleAdvector.py, which creates an output file.
-        Uses this output file to compute LCSs. 
-    Args:
-        lons    [list]      :   A list [lon1, lon2], where lon1 is bottom left corner of domain and lon2 is top right corner of domain. 
-        lats    [list]      :   A list [lat1, lat2], where lat1 is bottom left corner of domain and lat2 is top right corner of domain.
-        ts      [float]     :   Time step for integration, given in seconds
-        sep     [int]       :   Initial separation between particles, given in meters.
-        dur     [int]       :   Duration of integration, given in hours
-        date    [str]       :   A string of the date, in format 'ymd', no spacings.
-        member  [int]       :   Number of ensemble member, between 0-23.
-        output  [str]       :   Name of output file. Particle positions are saved to output.nc, LCS are saved to output_LCS.nc
-        at_time [int]       :   Which hour of the day LCSs are computed for
-    """
-    advector = Advection(file, lons, lats, ts, sep, dur, start)
-    parts = advector.run()
-    LCS = FTLE(parts, outfile=output, model_file=file, RLCS=True)
-
-def Run(output, start, file, dur):
-    """
-        Runs the particle and LCS simulation
-    Args:
-        i       [int]   :   The ensemble member for which LCSs should be computed
-        date    [str]   :   A string of the date, in format 'ymd', no spacings.
-        at_time [int]   :   Which hour of the day LCSs are computed for
-    """
-    CreateLCSField(file, lons=[25, 40], lats=[67,84], ts=3600, sep=2500, dur=dur, output=output, start=start)
 
 def CreateFTLEField(file, lons, lats, ts, sep, dur, ftle_output, start, particle_outfile=None, ensemble_member = None, RLCS=False, z=0, proj4=None):
     advector = Advection(file, lons, lats, ts, sep, dur, start, proj4=proj4)
